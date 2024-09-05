537120973778a70fb2093fe93395455d440c4163
romoch82 <romoch37@gmail.com>
2024-09-05 20:19:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+number_of_floors = 1
+new_floor = 1
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = int(number_of_floors)
+
+
+    def __str__(self):
+        return self.name
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
